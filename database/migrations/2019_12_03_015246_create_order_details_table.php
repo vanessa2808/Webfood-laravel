7b41475724af86155ca9aedaf4cd967e19f3ea92
VanessaNguyen <yenrion9941@gmail.com>
2019-12-03 09:42:23 +0700

    [create][]create project

---
+++ database/migrations/2019_12_03_015246_create_order_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('orderid');
+            $table->string('productid');
+            $table->string('quantity');
+            $table->string('price');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
