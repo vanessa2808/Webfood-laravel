336345b052502eb93986322f2b0ef524f432faf2
VanessaNguyen <yenrion9941@gmail.com>
2020-01-03 08:47:27 +0700

    deploy

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Str;
 return [
+    $DATABASE_URL=parse_url('postgres://dxzmkwfzusatoo:76ad73e0177c2e0d1264e0c96fdaca3b95af537a7a9be391305d59fd77fe5bb7@ec2-174-129-33-27.compute-1.amazonaws.com:5432/d1ml8kn5ge472c'),
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -11,7 +12,7 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -57,11 +58,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["password"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', 'page\HeaderController@index' , function () {
+Route::get('/home' , function () {
     return view('welcome');
 });
 
