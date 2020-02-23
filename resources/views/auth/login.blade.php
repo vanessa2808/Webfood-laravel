e688bc74c01a4dad5617ee99ff187183d6c4125c
VanessaNguyen <yenrion9941@gmail.com>
2020-02-23 12:26:20 +0700

    update v2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h2 class="contact-title">Register</h2>
+                    <h2 class="contact-title">Login</h2>
                 </div>
                 <div class="col-lg-8">
                     <form class="form-contact contact_form" action="{{ route('login') }}" method="POST"  enctype="multipart/form-data">
